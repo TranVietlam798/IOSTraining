0b2187f57cd8bd2ad0b2fa58b9a1fe24544738a0
lam.tran <lam.tran@hitachivantara.com>
2023-02-20 13:47:34 +0700

    Initial Commit

---
+++ IOSTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSTraining
+//
+//  Created by VIETLAMTR on 20/02/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
